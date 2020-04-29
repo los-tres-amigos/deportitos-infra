70409b0506cd687372308a7a7cb5931bcb029ef0
Carlos Fdez. Llamas <hello@sirikon.me>
2020-04-29 20:48:51 +0200

    borrar fallo

--- src/etc/workr/jobs/deportitos-back-build.sh
+++ src/etc/workr/jobs/deportitos-back-build.sh
@@ -4,7 +4,3 @@ set -euo pipefail
 git clone https://github.com/los-tres-amigos/deportitos-back.git
 cd deportitos-back
 mvn package
-
-echo "###################################################"
-echo "Este fallo realmente está simulado para demostrar que Peridot funciona"
-exit 1
