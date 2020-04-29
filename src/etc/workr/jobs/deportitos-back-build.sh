3cbf87864527c2eab537cb1a88ae3ce687672dd5
Carlos Fdez. Llamas <hello@sirikon.me>
2020-04-29 20:37:02 +0200

    new job

---
+++ src/etc/workr/jobs/deportitos-back-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+git clone https://github.com/los-tres-amigos/deportitos-back.git
+cd deportitos-back
+
+mvn package
+
+exit 1
--- src/etc/workr/jobs/example.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "Hello Peridot!"
-exit 1
