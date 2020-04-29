027f464a5d8f0126a0c0645050f3a40cfc7ac0ae
Carlos Fdez. Llamas <hello@sirikon.me>
2020-04-29 20:45:31 +0200

    add error

--- src/etc/workr/jobs/deportitos-back-build.sh
+++ src/etc/workr/jobs/deportitos-back-build.sh
@@ -4,3 +4,7 @@ set -euo pipefail
 git clone https://github.com/los-tres-amigos/deportitos-back.git
 cd deportitos-back
 mvn package
+
+echo "###################################################"
+echo "Este fallo realmente está simulado para demostrar que Peridot funciona"
+exit 1
